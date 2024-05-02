7811e2f128961443346931f9cfd5cc3ff1ee1068
lutfiangg20 <lutfiangg20@gmail.com>
2024-05-02 17:02:42 +0700

    test

--- src/app/components/layouts/Navbar.tsx
+++ src/app/components/layouts/Navbar.tsx
@@ -1,5 +1,42 @@
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between p-4">
+      <div className="flex justify-center w-64">
+        <img src="" alt="logo" />
+      </div>
+      <div>
+        <ul className="flex flex-row gap-5 mx-4">
+          <li>
+            <a href="">menu1</a>
+          </li>
+          <li>
+            <a href="">menu2</a>
+          </li>
+          <li>
+            <a href="">menu3</a>
+          </li>
+          <li>
+            <a href="">menu4</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
